b8b5827982e2dcd8eb000ca7ac591da32943f22b
Michael Long Desktop <michael@foolish-mortals.net>
2022-10-26 16:26:58 -0600

    Finished script to read all filenames in subfolders
    Added stripping of unneeded absolute paths

--- Voice-Acting/GenerateTextFileFromFilenames.bat
+++ Voice-Acting/GenerateTextFileFromFilenames.bat
@@ -1,4 +1,22 @@
 @echo off
+
+:: COMMENT, get all paths for subfolders
 >path_to_all_voicelines.txt (
   forfiles /s /m *.mp3 /c "cmd /c echo @path"
 )
+
+echo Got paths, now stripping
+
+:: COMMENT, strip out unneeded absolute path to reduce file size
+SET mypath=%~dp0
+echo %mypath%
+
+call:DoReplace "%mypath%" "" path_to_all_voicelines.txt path_to_all_voicelines.txt
+exit /b
+
+:DoReplace
+echo ^(Get-Content "%3"^) ^| ForEach-Object { $_.replace(%1,%2) } ^| Set-Content %4>Rep.ps1
+Powershell.exe -executionpolicy ByPass -File Rep.ps1
+if exist Rep.ps1 del Rep.ps1
+
+echo Finished all
